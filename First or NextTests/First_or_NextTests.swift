bc6ddecc2bc52d43a3d78cb652942c0acd11d69e
Nicholas Largen <nicholaslargen@nicholass-macbook-pro-3.local>
2015-12-15 05:59:22 -0600

    Initial Commit

---
+++ First or NextTests/First_or_NextTests.swift
@@ -0,0 +1,36 @@
+//
+//  First_or_NextTests.swift
+//  First or NextTests
+//
+//  Created by Nicholas Largen on 12/15/15.
+//  Copyright © 2015 Nicholas Largen. All rights reserved.
+//
+
+import XCTest
+@testable import First_or_Next
+
+class First_or_NextTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
